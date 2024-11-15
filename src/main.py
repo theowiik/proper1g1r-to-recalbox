57cd304eb37138d72b3673e77c86919ecdd1c9f0
theowiik <theo.le.wiik@gmail.com>
2024-11-15 23:36:09 +0100

    Extract contents from zip file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import json
+import zipfile
 from argparse import ArgumentParser, Namespace
 
 from mapping_validator import assert_mapping
-from util import get_files_by_extension, get_subdirs
+from util import get_files_by_extension, get_subdirs, unzip_at
 
 
 def get_mappings() -> dict[str, str]:
@@ -24,12 +25,20 @@ def main() -> None:
     recalbox_dirs_path = "recalbox_rom_directory"
 
     mappings = get_mappings()
+    mappings_filtered = {k: v for k, v in mappings.items() if v is not None}
     proper_zips = get_files_by_extension(input_dir, ".zip")
     recalbox_dirs = get_subdirs(recalbox_dirs_path)
 
     if not assert_mapping(mappings, proper_zips, recalbox_dirs):
         print("\nPlease fix the errors above and try again.")
-        exit(1)
+        # exit(1)
+
+    for proper_zip, recalbox_dir in mappings_filtered.items():
+        unzip_at(
+            f"{input_dir}/{proper_zip}",
+            "/mnt/d/repos/proper1g1r-to-recalbox/.ignore",
+            # f"{recalbox_dirs_path}/{proper_zip}"
+        )
 
 
 if __name__ == "__main__":
--- src/util.py
+++ src/util.py
@@ -1,4 +1,5 @@
 import os
+import zipfile
 
 
 def get_files_by_extension(directory: str, extension: str) -> set[str]:
@@ -19,3 +20,19 @@ def get_subdirs(directory: str) -> set[str]:
             subdirs.append(filename)
 
     return set(subdirs)
+
+
+def unzip_at(zip_path: str, output_dir: str) -> None:
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        files = zip_ref.namelist()
+        total_files = len(files)
+
+        if total_files == 0:
+            print("❌ The ZIP archive is empty.")
+            return
+
+        for index, file in enumerate(files, start=1):
+            zip_ref.extract(file, output_dir)
+            print(f"📂 Extracted: {file} ({index}/{total_files})")
+
+        print(f"✅ Completed extraction of {zip_path} to {output_dir}")
